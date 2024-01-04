bd65fda6d4fae898ba69a2b0d90ddec75625c561
kylaerod <kylarodriguez52@gmail.com>
2024-01-04 04:25:50 -0800

    complete with bugs

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# weatherAPIChallenge
+# My Weather Application
+
+## Description
+
+This project is to allow the user to view the weather, time and overall environment where they currently reside. They should be able to refresh the page view the conditions outside updated accordingly. There is also a search bar that is used to search up other cities' conditions.
+
+## Installation
+
+This project was completed from scratch using HTML, CSS, and JavaScript. 
+
+## Usage
+
+By starting with a basic skeleton, you then add the Open Weather Map API for the function of the weather app.
+
+![View of Page](<Screenshot (95).png>)
+
+## Credits
+
+Code that was referenced by Asish George Tech on YouTube. He was a helpful guide with the project and the video reference is linked below.
+
+https://www.youtube.com/watch?v=6trGQWzg2AI
+
+The API used was Open Weather Map. A link to that page is listed below.
+
+https://openweathermap.org/api
+
+Old projects I have completed helped a lot with this project as well.
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Kyla
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -2,50 +2,138 @@ const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
 const currentWeatherItems = document.getElementById('current-weather-items');
 const timezone = document.getElementById('timezone');
-const countryEl =document.getElementById('country');
-const weatherForecastEl =document.getElementById('weather-forecast');
-const currentTempEl =document.getElementById('current-temp');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
 
-
-const days = {'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'}
-const months = {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'}
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const API_Key = 'e05bf99b4ea4f2b50aa6227820271560';
 
-setInterval{() => {
+setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
-    const hoursInHrFormat = hour >= 13 ? hour %12: hour
+    const hoursInHrFormat = hour >= 13 ? hour % 12 : hour;
     const minutes = time.getMinutes();
-    const ampm = hours >=12 ? 'PM' : 'AM'
+    const ampm = hour >= 12 ? 'PM' : 'AM';
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ''+ <span 
-    id="am-pm">$(ampm)</span>$
+    timeEl.innerHTML = `${hoursInHrFormat < 10 ? '0' + hoursInHrFormat : hoursInHrFormat}:${minutes < 10 ? '0' + minutes : minutes} <span id="am-pm">${ampm}</span>`;
+    dateEl.innerHTML = `${days[day]}, ${date} ${months[month]}`;
+}, 1000);
 
-    dateEl.innerHTML = days[day] + ',' + date+ ' '+ months[month]
+getWeatherData();
 
-}, 1000};
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        let { latitude, longitude } = success.coords;
 
-getWeatherData ()
-function getWeatherData () {
-    navigator.geolocation.getCurrentPosition(success)=> {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_Key}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                showWeatherData(data);
+            });
+    });
+}
 
-        let {latitude, longitude} = success.coords;
+function showWeatherData(data) {
+    let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
+   
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + 'N' + data.lon + 'E';
+   
+    currentWeatherItems.innerHTML = '';
 
-        fetch ('https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units-metric&appid=${API_key}').then(res => res.json()).then(data => {
+    data.daily.forEach((day, idx) => {
+        if (idx !== 0) {
+            let weatherIcon = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+            let dayName = days[new Date(day.dt * 1000).getDay()];
+            let dayTemperature = day.temp.day;
+            let nightTemperature = day.temp.night;
 
-        console.log(data)
-        showWeatherData(data);
-        })
+            currentWeatherItems.innerHTML += `
+                <div class="weather-forecast-item">
+                    <img src="${weatherIcon}" alt="weather icon" class="w-icon">
+                    <div class="day">${dayName}</div>
+                    <div id="temp">Day - ${dayTemperature}&#176; F</div>
+                    <div id="temp">Night - ${nightTemperature}&#176; F</div>
+                </div>`;
+        }
+    });
 
+    let otherDayForecast = '';
+    data.daily.forEach((day, idx) => {
+        if (idx == 0) {
+            currentTempEl.innerHTML = `
+                <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+                <div class="other">
+                    <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
+                    <div id="temp">Day - ${day.temp.day}&#176; F</div>
+                    <div id="temp">Night - ${day.temp.night}&#176; F</div>
+                </div>`;
+        } else {
+            otherDayForecast += `
+                <div class="weather-forecast">
+                    <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
+                    <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                    <div id="temp">Day - ${day.temp.day}&#176; F</div>
+                    <div id="temp">Night - ${day.temp.night}&#176; F</div>
+                </div>`;
+        }
+    });
+
+    weatherForecastEl.innerHTML = otherDayForecast;
+}
+
+// For the Search Bar
+
+const searchInput = document.getElementById('searchInput');
+const searchHistoryContainer = document.getElementById('searchHistory');
+
+// Load search history from local storage
+let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+// Display search history
+function displaySearchHistory() {
+  searchHistoryContainer.innerHTML = '';
+  searchHistory.forEach(item => {
+    const historyItem = document.createElement('div');
+    historyItem.textContent = item;
+    historyItem.classList.add('history-item');
+    historyItem.addEventListener('click', () => {
+      searchInput.value = item;
+      search();
+    });
+    searchHistoryContainer.appendChild(historyItem);
+  });
+}
+
+// Save search history to local storage
+function saveSearchHistory() {
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+}
+
+// Make the Search
+function search() {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm !== '') {
+    // Perform the actual search logic here
+
+    // Update search history
+    if (!searchHistory.includes(searchTerm)) {
+      searchHistory.push(searchTerm);
+      saveSearchHistory();
+      displaySearchHistory();
     }
+
+    // Clear the search input
+    searchInput.value = '';
+  }
 }
 
-function  showWeatherData(data){
-    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
-    
-
-}
+// Initial display of search history
+displaySearchHistory();
